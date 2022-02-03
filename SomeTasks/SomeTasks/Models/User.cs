204695cc49629fd6d2a98244ed1f3998f5050bd7
ZotovaLera <90097489+Ler4i@users.noreply.github.com>
2022-02-03 20:25:22 +0300

    /

--- SomeTasks/SomeTasks/Models/User.cs
+++ SomeTasks/SomeTasks/Models/User.cs
@@ -7,7 +7,6 @@ namespace SomeTasks.Models
 {
     public class User
     {
-        public int Id { get; set; }
         public int UserId { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
