7645f12cf90244a7270d68b8b86f952ec1f3b43f
ZotovaLera <90097489+Ler4i@users.noreply.github.com>
2022-03-24 11:39:34 +0300

    IContext

---
+++ SomeTasks/SomeTasks/Models4/Interfaces/DbContext/IContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SomeTasks.Models4.Interfaces.DbContext
+{
+    interface IContext : IDisposable, IAsyncDisposable
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<Order> Orders { get; set; }
+        DbSet<Products> Products { get; set; }
+        DbSet<Prices> Prices { get; set; }
+        DbSet<Unit> Units { get; set; }
+    }
+}
